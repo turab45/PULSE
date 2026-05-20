4fb6184a90b33664842ac54e954f9b32c9ca570e
Turab <50545053+turab45@users.noreply.github.com>
2026-05-20 23:34:21 +0100

    Delete state.py

--- state.py
+++
@@ -1,7 +0,0 @@
-from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
-from langgraph.graph.message import add_messages
-
-
-class ChatState(TypedDict):
-    messages: Annotated[list[BaseMessage], add_messages]
